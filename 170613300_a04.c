049a5378039e371212b97d02f4822ec106c0b111
osc <ryan_ayala@hotmail.com>
2020-07-29 16:32:15 -0600

    Phase 3 finished off most of the functions and starting to move on to the 4 commands of RL RQ Run *

--- 170613300_a04.c
+++ 170613300_a04.c
@@ -11,10 +11,14 @@
 
 typedef struct thread //represents a single thread, you can add more members if required
 {
-	char tid[4];//id of the thread as read from file
-	unsigned int startTime;
-	pthread_t handle;
-	int retVal;
+        //id of the thread as read from file
+	int customer_num;
+        int* available;
+        int* max;
+        int* need; 
+        int* work;
+        int* finish;
+        int* allocation;
 } Thread;
 
 
@@ -26,24 +30,28 @@ int main(int argc,char* argv[]){
 int num_resources =  argc -1;
 
 
-int* avilable  = malloc(sizeof(int)*num_resorces); // allocated for avilable;
+int* available  = malloc(sizeof(int)*num_resorces); // allocated for avilable;
+
+pop_avail(argc,available, argv);
 
 
-int num_customers = numofcustormers( argc[1] 
+
+int num_customers = numofcustormers( argc[1]); 
 
 
 
 
 int* max = malloc( sizeof(int)*num_customers* num_resources);
 
+populatemax (argc[1], max, num_customers, num_resources );
 
 int* allocation = malloc(sizeof(int) *num_customers* num_resources);
 
-int* need = malloc(sizeof(int) * num_curomers* num_resources); 
+int* need = malloc(sizeof(int) * num_customers * num_resources); 
 
-intialize( allocation, numofcustomers, num_resources);
+intialize( allocation, num_customers, num_resources);
 
-intialize( need , numofcustomers, num_resources);
+intialize( need , num_customers, num_resources);
 
 
 
@@ -52,15 +60,95 @@ intialize( need , numofcustomers, num_resources);
 
 int*  work =  malloc(sizeof(int)*num_resorces);
 
-int*  finish = malloc(sizeof(int)*num_resources);
+int*  finish = malloc(sizeof(int)*num_customers);
+
+pop_avail(argc,work, argv); // filled available
+
+1d(finish, num_ customers); // filled finish
+
+
+printf("Number of Customers: %d \n",num_customers);
+
+printf("Currently Available resources:");
+
+for(int i = 0; i<num_resources;i++){
+	printf(" %d",available[i]);
+
+
+}
+
+printf("\n");
+
+printf("Maximum resources from file:\n");
+
+for(int i =0; i<num_customers;i++){
+
+for(int j =0; j<num_resources;j++){
+
+	if(j>0){
+	printf(",");
+	}
+printf("%d",max[i][j]);
+
+
+}
+
+printf("\n");
+}
+
+
+}
+
+
+
+Thread* threads = NULL;
+
+
+char  s[1025];
+printf("Enter Command : ");
+
+scanf("%s", s);
+
+
+if( strcmp(s,"*")==0){
+print1D(available, num_resources, "Available");
+
+print2D(max, num_customers, num_resources, "Max");
+
+
+print2D(allocation, num_customers, num_resources, "Allocation");
+
+
+print2D(need, num_customers, num_resources, "Need");
 
 
 
 
-} 
+}
+
+
+else if (strcmp(s,"RQ")==0){
 
 
 
+
+
+}
+
+
+else if(strcmp(s,"RL")==0){
+
+
+
+
+
+}
+
+
+else if(strcmp(s,"Run")==0){
+
+
+}
 
 
 
@@ -68,9 +156,26 @@ int*  finish = malloc(sizeof(int)*num_resources);
 int numofcustomers(char* fileName)//do not modify this method
 {
 	FILE *in = fopen(fileName, "r");
-	
 
+	int coustumer_count = 0;
 
+	char c; 
+	if(!in) {
+
+	printf("could not open file");
+         return -1;
+	 }
+
+	for (c =getc(in); c!= EFO ; c=getc(in)){
+
+	if(c== '\n'){
+
+	coustumer_count = coustumer_count + 1; 
+        }
+
+      }
+
+    fclose(in);
 
 	return coustumer_count;
 }
@@ -96,10 +201,36 @@ return;
 
 void populatemax (char * fileName, int*max, int n, int m ){
 
+ FILE *in = fopen(fileName, "r");
+
+  char c; 
+  if(!in){
+        printf("could not open file");
+         return -1;
+         }
+
+char* token = NULL;
+token =  strtok(lines[k],",");
+while(token!=NULL){
+
+
+	for( int i =0; i<n ; i++){
+
+		for( int j =0; j<m; j++ ){
+
+    			max[i][j] = atoi(token);
+   			token =  strtok(NULL,",");
+
+	    	}
+
+	   }
 
 
 
+	}
 
+fclose(in);
+return;
 }
 
 
@@ -118,3 +249,72 @@ for(int i =0;i <n; i++){
 return;
 
 }
+
+void pop_avail(int argc, int* available, char* argv){
+
+for (int z = 1;  z<agc ; z++ ){
+
+available[z-1] = atoi(argv[z]);
+
+}
+
+
+return;
+
+}
+
+
+
+
+void print1D(int* p, int n, char* des){
+
+printf("%s:\t", des);
+
+for( int i= 0; i < n; i++){
+
+printf("%d " , p[i]);
+}
+
+printf("\n");
+
+
+
+return;
+
+}
+
+
+
+void print2D(int *p, int n, int m, char*des){
+
+
+printf("%s:\n",des);
+
+for(int i =0; i<n;i++){
+
+for(int j =0; j<m;j++){
+
+        if(j>0){
+        printf(",");
+        }
+printf("%d",p[i][j]);
+
+
+}
+printf("\n");
+
+return; 
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+
