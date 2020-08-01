d2ba4b161430c637eaf21942955d03b038090ee8
osc <ryan_ayala@hotmail.com>
2020-07-31 21:10:01 -0600

    Phase 4 almost finished the Run, finished the safelty algorithm fucntion

--- 170613300_a04.c
+++ 170613300_a04.c
@@ -21,6 +21,10 @@ typedef struct thread //represents a single thread, you can add more members if 
         int* finish;
         int* allocation;
         sem_t* lock;
+        int num_resources;
+	int* safe;
+        int safe_size;
+	int* safe_index; 
 } Thread;
 
 
@@ -30,6 +34,8 @@ typedef struct thread //represents a single thread, you can add more members if 
 int main(int argc,char* argv[]){
 
 int num_resources =  argc -1;
+
+int  safe_index;
 
 
 int* available  = malloc(sizeof(int)*num_resorces); // allocated for avilable;
@@ -56,6 +62,7 @@ intialize( allocation, num_customers, num_resources);
 intialize( need , num_customers, num_resources);
 
 
+int* safe = malloc(sizeof(int)* num_customers);
 
 
 
@@ -152,24 +159,52 @@ pthread_t* threads = malloc(sizeof(pthread_t)*num_customers);
 
 
 int n =0;
+ Thread*  structure1 = malloc(sizeof(Thread));
+                          stucture1->customer_num = n;
+                          structure1->available = available;
+                          structure1->max = max;
+                          structure1->need= need;
+                          structure1->work = work;
+                         structure1->finish = finish; 
+                          structure1->allocation = allocation;
+                        structure1->num_resources = num_resources;
+                        structure1->safe = safe; 
+                        structure1->safe_size = 0;
+
+
 
 sem_t step_two;
 sem_init(&step_two, 0, 1);
+int  safe_num = safeSq(structure1);
+
+if(!safe_num){
+
+printf("There is no Safe sequence");
+}
+
+else{
+
+print1D(structure1->safe, structure1->num_customers,"Safe Sequence is";
+printf("Now going to executing the Threads:\n ");
 
 
 while(n<num_customers)
 		{
 
 			Thread*  structure = malloc(sizeof(Thread));
-                          stucture->customer_num = n;
+                          stucture->customer_num = structure1->safe[n];
   			  structure->available = available;
                           structure->max = max;
  			  structure->need= need;
 			  structure->work = work;
 			 structure->finish = finish; 
 			  structure->allocation = allocation;
-			structure->lock =&step_two; 
-			 pthread_create(&threads[n],NULL,threadRun,(void*) structure );
+			structure->lock =&step_two;
+                        structure->num_resources = num_resources;
+			structure->safe = safe; 
+			structure->safe_size = 0;
+			strucutre->safe_index =&safe_index; 
+ 			 pthread_create(&threads[structure1->safe[n],NULL,threadRun,(void*) structure );
    			n++;
 	}
 
@@ -187,9 +222,9 @@ pop_avail(argc,work, argv); // filled work
 
 
 
-
 }
-
+}
+}
 
 
 // reads the numer of rows
@@ -352,20 +387,107 @@ void* threadRun(void* t){     //implement this function i/n a suitable way
 
 Thread* p = (Thread*) t;
 
+while(true ){
+sem_wait(p->lock);
 
-while(true){
+if(p->safe[p->safe_index] == p-> customer_num){
 
-	sem_wait(p->lock);
+break;
+}
 
-       if(p->finish[p->customer_num]){
+sem_post(p->lock);
+sleep(.001);
 
-	break;
-	}
+} 
+printf(" ---> Customer/Thread  %d\n",p->customer_num);
+
+print1D(p->alloaction[p->customer_num],p->num_resources,"Allocated resources");
+
+print1D(p->need[p->customer_num],p->num_resources,"Needed");
+
+print1D(p->available[p->customer_num],p->num_resources,"Available");
+
+
+printf(" Thread has started");
+// work to  no deallolcation 
+
+
+printf("Thread is finished\n");
+printf("Thread  is relasing resources\n"):
+
+//dealocate
+print1D(p->available[p->customer_num],p->num_resources,"New Available");
+sem_post(p->lock);
+
+
+
 
 
 
 }
 
 
+
+int safeSq(structure*  strut ){
+
+
+
+for(int i = 0; i < strut->num_customers;i++){
+
+	int is_available = 1;
+	for(int k  =0; k< strut->num_resources; k++){
+
+			if(strut->need[i][k] > strut->available[k]{
+
+			is_available= 0;
+				break;
+			}
+
+
+
+	}
+
+
+	if(!strut->finish[i] && is_available){
+
+		strut->finish[i] = 1;
+
+ 
+
+
+		for(int j = 0; j< strut->num_resources;  j++){
+
+			strut->available[j] +=  strut->allocated[i][j]
+		}
+
+		strut->safe[safe_size] = i;
+
+		safe_size++:
+
+		
+		if(safeSq(strut)){
+			return 1;
+		}	
+
+		safe_size--;
+
+		strut->finish[i] = 0;
+
+		for(int  p=0; p<strut->resources; p++){
+
+			strut->available[p] -= strut->allocated[i][p];
+		}
+
+	}
+
+
+} 
+
+if(safe_size = strut->num_customers){
+
+  return 1;
+}
+
+return 0;
 
 }
